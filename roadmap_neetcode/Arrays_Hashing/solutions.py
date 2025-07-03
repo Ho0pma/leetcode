a50fe75086cb8977f731df6198bd654be8ba2602
Sasha Peklov <a.peklov@uptrader.io>
2025-07-03 23:46:12 +0300

    1 tasks

--- roadmap_neetcode/Arrays_Hashing/solutions.py
+++ roadmap_neetcode/Arrays_Hashing/solutions.py
@@ -1,10 +1,11 @@
 from typing import List
-from collections import Counter
+from collections import Counter, defaultdict
+
 
 # 217. Contains Duplicate
 
 # Задача: есть список nums. Если в нем есть дубликаты - вернуть True, если нет - False
-
+#
 # 1) Time O(n) Space O(n)
 # class Solution:
 #     def containsDuplicate(self, nums: List[int]) -> bool:
@@ -23,7 +24,7 @@ from collections import Counter
 # print(s.containsDuplicate(nums=[1, 2, 3, 1])) # true
 # print(s.containsDuplicate(nums=[1, 2, 3, 4])) # false
 # print(s.containsDuplicate(nums=[1, 1, 1, 3, 3, 4, 3, 2, 4, 2])) # true
-
+#
 # 2) Time O(n) Space O(n)
 # -- То же самое, только чуть лучше написано
 # class Solution:
@@ -46,10 +47,10 @@ from collections import Counter
 #####################################################################################################
 
 # 242. Valid Anagram
-
+#
 # Задача: подается две строки s и t. Нужно определить являются ли они анаграммой (когда из букв
 # одного слова можно составить другие, длина должна совпадать)
-
+#
 # 1) Time O(n) Space O(1)
 # class Solution:
 #     def isAnagram(self, s: str, t: str) -> bool:
@@ -62,7 +63,7 @@ from collections import Counter
 # s = Solution()
 # print(s.isAnagram(s="anagram", t="nagaram"))  # true
 # print(s.isAnagram(s="rat", t="car"))  # false
-
+#
 # 2) Time O(n) Space O( 1)
 # -- не используя Counter
 # class Solution:
@@ -92,8 +93,87 @@ from collections import Counter
 
 #####################################################################################################
 
+# 1. Two Sum
 #
+# Задача: подается список интов (nums) и target. Нужно найти такие два инта, которые в сумме дают
+# target. Вернуть индексы этих чисел
+#
+# 1) Time O(n) Space O(n)
+# -- two pass hash table
+# class Solution:
+#     def twoSum(self, nums: List[int], target: int) -> List[int]:
+#         hash_map = {}
+#
+#         for i in range(len(nums)):
+#             hash_map[nums[i]] = i
+#
+#         for i in range(len(nums)):
+#             diff = target - nums[i]
+#             if diff in hash_map and i != hash_map[diff]:
+#                 return [i, hash_map[diff]]
+#
+#         return []
+#
+#
+# s = Solution()
+# print(s.twoSum(nums=[2, 7, 11, 15], target=9))  # [0,1]
+# print(s.twoSum(nums=[3, 2, 4], target=6))  # [1,2]
+# print(s.twoSum(nums=[3, 3], target=6))  # [0,1]
+#
+# 2) Time O(n) Space O(n)
+# -- one pass hash table
+# class Solution:
+#     def twoSum(self, nums: List[int], target: int) -> List[int]:
+#         hash_map = {}
+#
+#         for i in range(len(nums)):
+#             diff = target - nums[i]
+#
+#             if diff in hash_map:
+#                 return [i, hash_map[diff]]
+#             hash_map[nums[i]] = i
+#
+#         return []
+#
+#
+# s = Solution()
+# print(s.twoSum(nums=[2, 7, 11, 15], target=9))  # [0,1]
+# print(s.twoSum(nums=[3, 2, 4], target=6))  # [1,2]
+# print(s.twoSum(nums=[3, 3], target=6))  # [0,1]
+
+#####################################################################################################
+
+# 49. Group Anagrams
 
 # Задача:
 
-# 1) Time O(n) Space O(1)
+# PRAC:
+# 1)
+# d = {}
+# d['a'].append(1) # error тк нет а: []
+# print(d)
+# 2)
+# d = {'a': []}
+# d['a'].append(1) # {'a': [1]} --> добавит тк уже был ключ  'a'
+# print(d)
+# 3)
+# d = defaultdict(list) # если ключа еще не было в дикте - автоматически создаст для него значение list
+# d['a'].append(1) # defaultdict(<class 'list'>, {'a': [1]})
+# print(d)
+# d['b'] = 1 # не будет создавать значение типа list, просто создаст 'b': 1
+# print(d) # defaultdict(<class 'list'>, {'a': [1], 'b': 1})
+
+
+# 1) Time O(n) Space O(n)
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+
+
+
+
+
+
+s = Solution()
+print(s.groupAnagrams(strs=["eat", "tea", "tan", "ate", "nat", "bat"])) # Output: [["bat"],["nat","tan"],["ate","eat","tea"]]
+
+
