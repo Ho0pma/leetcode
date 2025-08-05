4ae33aa82b5480f3293e973c9da480560727a7d7
Sasha Peklov <a.peklov@uptrader.io>
2025-08-05 18:58:08 +0300

    wip

--- roadmap_neetcode/Arrays_Hashing/solutions_with_tests.py
+++ roadmap_neetcode/Arrays_Hashing/solutions_with_tests.py
@@ -4,6 +4,7 @@ from collections import Counter, defaultdict
 
 import pytest
 
+
 # 🌶️ 242. Valid Anagram
 #
 # Задача: подается две строки s и t. Нужно определить являются ли они анаграммой (когда из букв
@@ -71,3 +72,77 @@ import pytest
 
 ########################################################################################################################
 
+# 🌶️ 49. Group Anagrams
+
+# Задача: подается список (strs), состоящий из слов. Нужно составить новый список, комбинирующий анаграммы
+# вместе, см пример в конце, сразу понятно.
+
+# 1) Time O(n * k) Space O(n * 26)
+# class Solution:
+#     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+#         print()
+#         res = defaultdict(list)
+#         for word in strs:
+#             char_counter_list = [0] * 26
+#             for i in word:
+#                 char_counter_list[(ord(i) - ord('a'))] += 1
+#
+#             res[tuple(char_counter_list)].append(word)
+#
+#         return list(res.values())
+
+# 2) плохой вар из-за сортировки
+# TIME: O(N * K log K)
+# N — количество слов
+# K — средняя длина слова
+# Counter — O(K)
+# sorted(items()) — O(K log K)
+# SPACE: O(N * K)
+# K - кол-во созданных тюплов
+# N - кол-во слов
+# class Solution:
+#     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+#         print()
+#         res = defaultdict(list)
+#         for word in strs:
+#             char_counter_list = sorted(Counter(word).items())
+#             print(char_counter_list)
+#
+#             res[tuple(char_counter_list)].append(word)
+#         print(res)
+#
+#         return list(res.values())
+#
+# def sort_output(anagram_list):
+#     for i in anagram_list:
+#         i.sort()
+#     anagram_list.sort()
+#
+#     return anagram_list
+#
+# @pytest.mark.parametrize(
+#     "strs, expected",
+#     [
+#         (["eat", "tea", "tan", "ate", "nat", "bat"], [["bat"], ["nat", "tan"], ["ate", "eat", "tea"]]),
+#         ([""], [[""]]),
+#         (["a"], [["a"]]),
+#     ]
+# )
+# def test_groupAnagrams(strs, expected):
+#     s = Solution()
+#     assert sort_output(s.groupAnagrams(strs)) == sort_output(expected)
+
+#####################################################################################################
+
+# 🌶️ 347. Top K Frequent Elements
+
+# Задача: подается список интов (nums) и таргет (k). Нужно найти k максимальных вхождений в массив
+# и вывести эти числа
+# те если nums=[4, 1, -1, 2, -1, 2, 3] и k=2
+# Counter({-1: 2, 2: 2, 4: 1, 1: 1, 3: 1}) -- топ два вхождения имеют -1 и 2
+
+# Time O(n log n)  Space O(n) (за хранение counter)
+# 1) sorting + Counter
+# class Solution:
+#     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+#         pass
