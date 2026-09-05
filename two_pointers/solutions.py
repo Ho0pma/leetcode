9ebc3d4718d5aa2b23edd0e5926950a48b303c76
Sasha Peklov <a.peklov@uptrader.io>
2026-09-05 20:45:59 +0300

    resolved 1 task

--- two_pointers/solutions.py
+++ two_pointers/solutions.py
@@ -859,6 +859,9 @@ class ListNode:
 # Complexity: Time: O(n + m), Memory O(n + m)
 # class Solution:
 #     def intersection(self, nums1, nums2):
+#         # создать сет на основе первого списка
+#         # пройтись по второму списку, проверить есть ли в сете число
+#         # если есть - удалить из сета - занести в список
 #         set1 = set(nums1)
 #         result = []
 #         for x in nums2:
@@ -885,4 +888,52 @@ class ListNode:
 
 # ----------------------------------------------------------------------------------------------------------------------#
 
-# 14. ☠️
+# 14. ☠️ 350. Intersection of Two Arrays II
+
+# task: Given two integer arrays nums1 and nums2, return an array of their intersection.
+#       Each element in the result must appear as many times as it shows in both arrays
+#       and you may return the result in any order.
+
+# Example 1:
+# Input: nums1 = [1,2,2,1], nums2 = [2,2]
+# Output: [2,2]
+#
+# Example 2:
+# Input: nums1 = [4,9,5], nums2 = [9,4,9,8,4]
+# Output: [4,9]
+# Explanation: [9,4] is also accepted.
+
+# 🌶️ best approach
+# Complexity: Time: O(n + m), Memory O(n + m)
+# class Solution:
+#     def intersect(self, nums1: List[int], nums2: List[int]) -> List[int]:
+#         # создать хэш мапу (элементы: кол-во) по первому списку
+#         # пройтись по второму списку, проверяя есть ли число в хэш мапе
+#         # если есть уменьшать счетчик
+#
+#         # 1) c = Counter(nums1)
+#         # 2) c = {}.fromkeys(nums1, 0)
+#         # 3)
+#         c = {}
+#         for i in nums1:
+#             if i in c:
+#                 c[i] += 1 # или так: c[i] = c.get(i, 0) + 1
+#             else:
+#                 c[i] = 1
+#
+#         result = []
+#         for i in nums2:
+#             if i in c and c[i] != 0:
+#                 result.append(i)
+#                 c[i] -= 1
+#
+#         return result
+#
+#
+# s = Solution()
+# print(s.intersect(nums1=[1, 2, 2, 1], nums2=[2, 2]))  # [2,2]
+# print(s.intersect(nums1=[4, 9, 5], nums2=[9, 4, 9, 8, 4]))  # [4,9]
+
+# ----------------------------------------------------------------------------------------------------------------------#
+
+# 15. ☠️
